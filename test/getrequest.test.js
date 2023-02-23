8ad4e4f9348cb662ed9242fec361dd7e1efa2a12
olpapina <opapina@solvd.com>
2023-02-23 10:38:18 +0300

    carina api tests were created

---
+++ test/getrequest.test.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const assert = require('assert');
+const fs = require('fs');
+const chai = require('chai');
+chai.use(require('chai-json-schema'));
+describe('GET method tests', () => {
+        let rawdata = fs.readFileSync('./test/resources/get_rs.json');
+        let expectedData = JSON.parse(rawdata);
+        let expectedSchema = JSON.parse(fs.readFileSync('./test/resources/get_rs.schema'));
+        it('Return 10 users from https://jsonplaceholder.typicode.com/', async function () {
+                await axios({
+                        method: 'GET',
+                        url: 'https://jsonplaceholder.typicode.com/users',
+                        params: {
+                                _limit: 10
+                        }
+                })
+                        .then(function (response) {
+                                let expectedDataString = JSON.stringify(expectedData);
+                                let actualDataString = JSON.stringify(response.data);
+                                assert.equal(response.status, 200);
+                                assert.equal(response.statusText, 'OK');
+                                assert.equal(actualDataString, expectedDataString);
+                                chai.expect(response.data).to.be.jsonSchema(expectedSchema);
+                        })
+                        .catch((error) => Promise.reject(error));
+        });
+});
---
+++ test/postrequest.test.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const assert = require('assert');
+const fs = require('fs');
+const chai = require('chai');
+chai.use(require('chai-json-schema'));
+
+describe('POST method tests', () => {
+    let postRequest = JSON.parse(fs.readFileSync('./test/resources/post_rq.json'));
+    let expectedResponse = JSON.parse(fs.readFileSync('./test/resources/post_rs.json'));
+    let expectedSchema = JSON.parse(fs.readFileSync('./test/resources/post_rs.schema'));
+
+    it('Create new user on the https://jsonplaceholder.typicode.com/users', async function () {
+        await axios({
+            method: 'POST',
+            url: 'https://jsonplaceholder.typicode.com/users',
+            data: postRequest
+        })
+            .then(function (response) {
+                let expectedDataString = JSON.stringify(expectedResponse);
+                let actualDataString = JSON.stringify(response.data);
+                assert.equal(response.status, 201);
+                assert.equal(response.statusText, 'Created');
+                assert.deepEqual(response.data.id, 11);
+                assert.equal(actualDataString, expectedDataString);
+                chai.expect(response.data).to.be.jsonSchema(expectedSchema);
+            })
+            .catch((error) => Promise.reject(error));
+    });
+    it('Create new user with mising name and username', async function () {
+        delete postRequest.name;
+        delete postRequest.username;
+        await axios({
+            method: 'POST',
+            url: 'https://jsonplaceholder.typicode.com/users',
+            data: postRequest
+        })
+            .then(function (response) {
+                assert.equal(response.status, 201);
+                assert.equal(response.statusText, 'Created');
+                assert.deepEqual(response.data.id, 11);
+                chai.expect(response.data).to.be.jsonSchema(expectedSchema);
+                chai.expect(response.data).to.not.have.property("name");
+                chai.expect(response.data).to.not.have.property("username");
+            })
+            .catch((error) => Promise.reject(error));
+    });
+});
